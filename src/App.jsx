ac76080d6470a126d121afe646607b37706a01f6
suffynux <suffynux@gmail.com>
2025-08-03 20:27:02 +0500

    it service fixed

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import About from "./Components/AboutUS/About";
 import Services from "./Components/OurServices/Services";
 import Contact from "./Components/ContactUs/Contact";
 import { Italic } from "lucide-react";
-import ITServices from "./Components/OurServices/ItServices";
 import TechRecruitment from "./Components/OurServices/TechRecruitment";
 import ITManagedServices from "./Components/OurServices/ITManagedServices";
 import ITAssetManagement from "./Components/OurServices/ITAssetManagement";
 import NetworkUserSupport from "./Components/OurServices/NetworkUserSupport";
 import ProjectChangeManagement from "./Components/OurServices/ProjectChangeManagement";
 import CreativeDigitalSupport from "./Components/OurServices/CreativeDigitalSupport";
+import ITServices from "./Components/OurServices/ItServices";
 
 
 function App() {
